c33dc5ea6e8eb45d568fef6e459434e003df3256
fanquake <fanquake@gmail.com>
2023-05-16 09:45:24 +0100

    Merge bitcoin/bitcoin#27665: walletdb: Remove unused CreateMockWalletDatabase
    0282b2126dcc1216a25417db0716a3a28489b72d walletdb: Remove unused CreateMockWalletDatabase (Andrew Chow)
    
    Pull request description:
    
      This has been superseded by the MockableDatabase. Remove to avoid confusion as to which type of mock database to use for testing.
    
      I thought this was included in #26715, maybe it got lost in a rebase.
    
    ACKs for top commit:
      furszy:
        utACK 0282b212
      brunoerg:
        crACK 0282b2126dcc1216a25417db0716a3a28489b72d
    
    Tree-SHA512: 18445c4d8a4e2609ef7471c6d75297f43694b79e768f6c993a5addb1dc0e88bd12bac263c9d8e903d828ddf6bf50434f9e2f72048f4fc528e98fed8ee65123ca

--- src/wallet/walletdb.h
+++ src/wallet/walletdb.h
@@ -267,10 +267,6 @@ bool ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue, st
 
 /** Return object for accessing dummy database with no read/write capabilities. */
 std::unique_ptr<WalletDatabase> CreateDummyWalletDatabase();
-
-/** Return object for accessing temporary in-memory database. */
-std::unique_ptr<WalletDatabase> CreateMockWalletDatabase(DatabaseOptions& options);
-std::unique_ptr<WalletDatabase> CreateMockWalletDatabase();
 } // namespace wallet
 
 #endif // BITCOIN_WALLET_WALLETDB_H
