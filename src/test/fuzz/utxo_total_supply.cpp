a1db4476bd442e72076d99a573c4c5f907c34634
fanquake <fanquake@gmail.com>
2023-06-07 15:47:50 +0200

    Merge bitcoin/bitcoin#27810: fuzz: Partially revert #27780
    71200ac390fe5c10f088cbe8053b010b515757b1 [fuzz] Only check duplicate coinbase script when block was valid (dergoegge)
    
    Pull request description:
    
      Partially revert #27780, because moving the duplicate coinbase check out of the `was_valid` branch leads to non-bug crashes in the fuzz target.
    
      For context and further explanation see: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=59516
    
    ACKs for top commit:
      MarcoFalke:
        nice lgtm ACK 71200ac390fe5c10f088cbe8053b010b515757b1
    
    Tree-SHA512: 8c38e5ff9de6331016b9a0c5e435d007d46186151b04c09085f617bb31627a28ad56678066fe152372a3ad8656f026439e3e2f9ee61d7ef588072aef8124eaa3

--- src/test/fuzz/utxo_total_supply.cpp
+++ src/test/fuzz/utxo_total_supply.cpp
@@ -147,12 +147,12 @@ FUZZ_TARGET(utxo_total_supply)
 
                 const auto prev_utxo_stats = utxo_stats;
                 if (was_valid) {
-                    circulation += GetBlockSubsidy(ActiveTip(), Params().GetConsensus());
-
                     if (duplicate_coinbase_height == ActiveHeight()) {
                         // we mined the duplicate coinbase
                         assert(current_block->vtx.at(0)->vin.at(0).scriptSig == duplicate_coinbase_script);
                     }
+
+                    circulation += GetBlockSubsidy(ActiveTip(), Params().GetConsensus());
                 }
 
                 UpdateUtxoStats();
