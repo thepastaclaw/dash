1e0e6b3fb47f8c384b8252567dc6c069fd11e7cd
fanquake <fanquake@gmail.com>
2023-06-12 16:38:48 +0100

    Merge bitcoin/bitcoin#27783: Add public Boost headers explicitly
    2484cacb7a6367b24e924dba0825c843b1dfc1c3 Add public Boost headers explicitly (Hennadii Stepanov)
    fade2adb5bb4ce9753e7f25da5fb1521f2f503ec test: Avoid `BOOST_ASSERT` macro (Hennadii Stepanov)
    
    Pull request description:
    
      To check symbols in the code base, run:
      ```
      git grep boost::multi_index::identity
      git grep boost::multi_index::indexed_by
      git grep boost::multi_index::tag
      git grep boost::make_tuple
      ```
    
      Hoping on the absence of conflicts with top-prio PRs :)
    
    ACKs for top commit:
      MarcoFalke:
        lgtm ACK 2484cacb7a6367b24e924dba0825c843b1dfc1c3
      TheCharlatan:
        ACK 2484cacb7a6367b24e924dba0825c843b1dfc1c3
    
    Tree-SHA512: d122ab028eee76ee1c4609ed51ec8db0c8c768edcc2ff2c0e420a48e051aa71e99748cdb5d22985ae6d97c808c77c1a27561f0715f77b256f74c1c310b37694c

--- src/wallet/test/wallet_tests.cpp
+++ src/wallet/test/wallet_tests.cpp
@@ -1574,11 +1574,10 @@ BOOST_FIXTURE_TEST_CASE(wallet_sync_tx_invalid_state_test, TestChain100Setup)
     }
 
     // Add tx to wallet
-    const auto& op_dest = wallet.GetNewDestination("");
-    BOOST_ASSERT(op_dest);
+    const auto op_dest{*Assert(wallet.GetNewDestination(""))};
 
     CMutableTransaction mtx;
-    mtx.vout.emplace_back(COIN, GetScriptForDestination(*op_dest));
+    mtx.vout.emplace_back(COIN, GetScriptForDestination(op_dest));
     mtx.vin.emplace_back(g_insecure_rand_ctx.rand256(), 0);
     const auto& tx_id_to_spend = wallet.AddToWallet(MakeTransactionRef(mtx), TxStateInMempool{})->GetHash();
 
