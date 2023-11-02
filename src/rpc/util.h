bb117fc246ee61e76f58c82d9895dd0661046ceb
Andrew Chow <github@achow101.com>
2023-11-02 15:37:51 -0400

    Merge bitcoin/bitcoin#28172: refactor: use string_view for passing string literals to Parse{Hash,Hex}
    bb91131d545d986aab81c4bb13676c4520169259 doc: remove out-of-date external link in src/util/strencodings.h (Jon Atack)
    7d494a48ddf4248ef3b1753b6e7f2eeab3a8ecb7 refactor: use string_view to pass string literals to Parse{Hash,Hex} (Jon Atack)
    
    Pull request description:
    
      as `string_view` is optimized to be trivially copiable, whereas the current code creates a `std::string` copy at each call.
    
      These utility methods are called by quite a few RPCs and tests, as well as by each other.
    
      ```
      $ git grep "ParseHashV\|ParseHashO\|ParseHexV\|ParseHexO" | wc -l
      61
      ```
    
      Also remove an out-of-date external link.
    
    ACKs for top commit:
      jonatack:
        Rebased per `git range-diff c9273f6 b94581a bb91131` for an include header from the merge of https://github.com/bitcoin/bitcoin/pull/28230. Should be trivial to re-ACK.
      maflcko:
        lgtm ACK bb91131d545d986aab81c4bb13676c4520169259
      ns-xvrn:
        ACK https://github.com/bitcoin/bitcoin/commit/bb91131d545d986aab81c4bb13676c4520169259
      achow101:
        ACK bb91131d545d986aab81c4bb13676c4520169259
      brunoerg:
        crACK bb91131d545d986aab81c4bb13676c4520169259
    
    Tree-SHA512: 9734fe022c9e43fd93c23a917770d332dbbd3132c80a234059714c32faa6469391e59349954749fc86c4ef0b18d5fd99bf8f4b7b82d9f799943799c1253272ae

--- src/rpc/util.h
+++ src/rpc/util.h
@@ -28,6 +28,7 @@
 #include <string>
 #include <type_traits>
 #include <utility>
+#include <string_view>
 #include <variant>
 #include <vector>
 
@@ -103,10 +104,10 @@ void RPCTypeCheckObj(const UniValue& o,
  * Utilities: convert hex-encoded Values
  * (throws error if not hex).
  */
-uint256 ParseHashV(const UniValue& v, std::string strName);
-uint256 ParseHashO(const UniValue& o, std::string strKey);
-std::vector<unsigned char> ParseHexV(const UniValue& v, std::string strName);
-std::vector<unsigned char> ParseHexO(const UniValue& o, std::string strKey);
+uint256 ParseHashV(const UniValue& v, std::string_view name);
+uint256 ParseHashO(const UniValue& o, std::string_view strKey);
+std::vector<unsigned char> ParseHexV(const UniValue& v, std::string_view name);
+std::vector<unsigned char> ParseHexO(const UniValue& o, std::string_view strKey);
 
 bool ParseBoolV(const UniValue& v, const std::string &strName);
 
