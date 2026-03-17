bf2eb4fd55065fe2a265cd5dbb139c5b3bbc31d7
PastaClaw <thepastaclaw@users.noreply.github.com>
2026-03-17 13:20:49 -0500

    lint: remove unused import assert_fee_amount from rpc_packages.py

--- test/functional/rpc_packages.py
+++ test/functional/rpc_packages.py
@@ -15,7 +15,6 @@ from test_framework.p2p import P2PTxInvStore
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
     assert_equal,
-    assert_fee_amount,
     assert_raises_rpc_error,
 )
 from test_framework.wallet import (
