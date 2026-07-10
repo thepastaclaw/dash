9a7b2a8e0371d6aca26023f525e780e62460baf7
Pasta <pasta@dashboost.org>
2026-07-10 11:49:48 -0500

    Merge #7259: fix: dangling point to cj client
    dc74e23dff48cac6f9d4761f73539cd33f66f448 fix: move is_mixing atomic from CCoinJoinClientManager to CWallet (Konstantin Akimov)
    8d5876ff99d12a42fd10cbd63466b590287f1fac fix: potential deadlock issue. cs_wallet_manager_map should be locked after cs_wallet (Konstantin Akimov)
    268bf4d91fd6683a2ee9acfc3ce5560b2ee7ede9 test: make newkeypool mixing test deterministically reproduce lock cycle (PastaClaw)
    5511e29300fd4a1183bebfabc1cceaff39ac7315 test: cover newkeypool CoinJoin callback behavior (PastaClaw)
    ef7b1f5e82ae7652e69a5a6e9ec7983097b76bc9 fix: don't auto start mixing for locked wallets (Konstantin Akimov)
    c7cc8ceda6507db52eced7a6a55406302580615b refactor: cleanups for CoinJoin client interface (UdjinM6)
    f8c71cb6c95eb534f696af919d20abaf0cd4e69f fix: keep flag `coinjoin` enabled consistently with user's intention over UI or command line (Konstantin Akimov)
    4424487a71370ef3d67ddb858d51c5620050a395 fix: call InitCoinJoin only wallet is added (Konstantin Akimov)
    07d5e3e96672d4b1e4a28a7d34987dc6e63eb8bd refactor: remove duplicated methods from CJWalletManagerImpl to prefer interface's (Konstantin Akimov)
    d34a4c1c619b97a00b9fdc933141b0515592b285 fix: avoid dangling pointers to CJ clients (Konstantin Akimov)
    0cb791956ac23dd67557b0eed3d493058a581c9a refactor: remove CoinJoinClientImpl wrapper and return bare pointer to interfaces::CoinJoin::Client (Konstantin Akimov)
    69b57abc922ecff0a2f4e20aeb34fdb1ccd0df40 fix: regression test coinjoin_options_tests by enforcing call of `interfaces::MakeCoinJoinLoader` (Konstantin Akimov)
    d6f8438c0a11d2ddf8d60597aef7b671558e0700 fix: linkage error for non-wallet builds for further changes (Konstantin Akimov)
    
    Pull request description:
    
      ## Issue being fixed or feature implemented
      Hold lock during flush operations to prevent TOCTOU race condition.
    
      getClient() releases cs_wallet_manager_map before returning the raw pointer. If another thread calls removeWallet() concurrently, the unique_ptr is destroyed while flushWallet() still holds a reference, leading to use-after-free. This is inconsistent with removeWallet() which safely erases under the lock.
    
      Discovered here https://github.com/dashpay/dash/pull/7248#discussion_r2981412759
    
      ## What was done?
      Use `doWithClient(name, [](){...})` instead of `getClient(name)->...`
    
      Mutex `cs_wallet_manager_map` should not be ever locked under `cs_wallet`.
    
      ## How Has This Been Tested?
      Run unit & functional tests
    
      ## Breaking Changes
      N/A
    
      ## Checklist:
      - [x] I have performed a self-review of my own code
      - [ ] I have commented my code, particularly in hard-to-understand areas
      - [ ] I have added or updated relevant unit/integration/functional/e2e tests
      - [ ] I have made corresponding changes to the documentation
      - [x] I have assigned this pull request to a milestone
    
    ACKs for top commit:
      kwvg:
        utACK dc74e23dff48cac6f9d4761f73539cd33f66f448
      PastaPastaPasta:
        utACK dc74e23dff48cac6f9d4761f73539cd33f66f448
    
    Tree-SHA512: 4030f52aa391c889607d06cdbedc8bbd735ccf628aefd93b37868fb41b0d609cade2229709b8798527cc4d20bc426022767dba776c8a4be5cffe8ec060eb1355

--- src/coinjoin/client.h
+++ src/coinjoin/client.h
@@ -8,6 +8,7 @@
 #include <coinjoin/coinjoin.h>
 #include <coinjoin/util.h>
 #include <evo/types.h>
+#include <interfaces/coinjoin.h>
 #include <msg_result.h>
 
 #include <net_types.h>
@@ -15,7 +16,6 @@
 #include <util/ranges.h>
 #include <util/translation.h>
 
-#include <atomic>
 #include <deque>
 #include <memory>
 #include <utility>
@@ -91,6 +91,8 @@ public:
     void Flush(const std::string& name) EXCLUSIVE_LOCKS_REQUIRED(!cs_wallet_manager_map);
 
     CCoinJoinClientManager* Get(const std::string& name) const EXCLUSIVE_LOCKS_REQUIRED(!cs_wallet_manager_map);
+    bool DoForClient(const std::string& name, const std::function<void(CCoinJoinClientManager&)>& func)
+        EXCLUSIVE_LOCKS_REQUIRED(!cs_wallet_manager_map);
 
     template <typename Callable>
     void ForEachCJClientMan(Callable&& func) EXCLUSIVE_LOCKS_REQUIRED(!cs_wallet_manager_map)
@@ -240,7 +242,7 @@ public:
 
 /** Used to keep track of current status of mixing pool
  */
-class CCoinJoinClientManager
+class CCoinJoinClientManager : public interfaces::CoinJoin::Client
 {
 private:
     const std::shared_ptr<wallet::CWallet> m_wallet;
@@ -254,8 +256,6 @@ private:
     // TODO: or map<denom, CCoinJoinClientSession> ??
     std::deque<CCoinJoinClientSession> deqSessions GUARDED_BY(cs_deqsessions);
 
-    std::atomic<bool> fMixing{false};
-
     int nCachedLastSuccessBlock{0};
     int nMinBlocksToWait{1}; // how many blocks to wait for after one successful mixing tx in non-multisession mode
     bilingual_str strAutoDenomResult;
@@ -263,15 +263,15 @@ private:
     // Keep track of current block height
     int nCachedBlockHeight{0};
 
+    int nCachedNumBlocks{std::numeric_limits<int>::max()};    // used for the overview screen
+    bool fCreateAutoBackups{true}; // builtin support for automatic backups
+
     bool WaitForAnotherBlock() const;
 
     // Make sure we have enough keys since last backup
     bool CheckAutomaticBackup();
 
 public:
-    int nCachedNumBlocks{std::numeric_limits<int>::max()};    // used for the overview screen
-    bool fCreateAutoBackups{true}; // builtin support for automatic backups
-
     CCoinJoinClientManager() = delete;
     CCoinJoinClientManager(const CCoinJoinClientManager&) = delete;
     CCoinJoinClientManager& operator=(const CCoinJoinClientManager&) = delete;
@@ -282,14 +282,6 @@ public:
     ~CCoinJoinClientManager();
 
     void ProcessMessage(CNode& peer, CChainState& active_chainstate, CConnman& connman, const CTxMemPool& mempool, std::string_view msg_type, CDataStream& vRecv) EXCLUSIVE_LOCKS_REQUIRED(!cs_deqsessions);
-
-    bool StartMixing();
-    void StopMixing();
-    bool IsMixing() const;
-    void ResetPool() EXCLUSIVE_LOCKS_REQUIRED(!cs_deqsessions);
-
-    std::vector<std::string> GetStatuses() const EXCLUSIVE_LOCKS_REQUIRED(!cs_deqsessions);
-    std::string GetSessionDenoms() EXCLUSIVE_LOCKS_REQUIRED(!cs_deqsessions);
 
     bool GetMixingMasternodesInfo(std::vector<CDeterministicMNCPtr>& vecDmnsRet) const EXCLUSIVE_LOCKS_REQUIRED(!cs_deqsessions);
 
@@ -314,7 +306,18 @@ public:
     void DoMaintenance(ChainstateManager& chainman, CConnman& connman, const CTxMemPool& mempool)
         EXCLUSIVE_LOCKS_REQUIRED(!cs_deqsessions);
 
-    void GetJsonInfo(UniValue& obj) const EXCLUSIVE_LOCKS_REQUIRED(!cs_deqsessions);
+    // interfaces::CoinJoin::Client overrides
+    void resetCachedBlocks() override { nCachedNumBlocks = std::numeric_limits<int>::max(); }
+    int getCachedBlocks() const override { return nCachedNumBlocks; }
+    void setCachedBlocks(int nCachedBlocks) override { nCachedNumBlocks = nCachedBlocks; }
+    void disableAutobackups() override { fCreateAutoBackups = false; }
+    void resetPool() override EXCLUSIVE_LOCKS_REQUIRED(!cs_deqsessions);
+    UniValue getJsonInfo() const override EXCLUSIVE_LOCKS_REQUIRED(!cs_deqsessions);
+    std::vector<std::string> getSessionStatuses() const override EXCLUSIVE_LOCKS_REQUIRED(!cs_deqsessions);
+    std::string getSessionDenoms() const override EXCLUSIVE_LOCKS_REQUIRED(!cs_deqsessions);
+    bool isMixing() const override;
+    bool startMixing() override;
+    void stopMixing() override;
 };
 
 #endif // BITCOIN_COINJOIN_CLIENT_H
